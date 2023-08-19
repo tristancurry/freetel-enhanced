771c97b90c9c02ffdca28f9320524170321f7704
Tristan Curry <7457323+tristancurry@users.noreply.github.com>
2023-08-19 15:44:52 +0930

    rendering now working sensibly
    can track a single electron across the 'experiment region'

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
+let canvas_exp = document.createElement('canvas');
+canvas_exp.setAttribute('width', 600);
+canvas_exp.setAttribute('height', 300);
+canvas_exp.style.backgroundColor = 'rgb(0,0,100)';
+document.body.appendChild(canvas_exp);
+
+let ctx_exp = canvas_exp.getContext('2d');
+
+
+
 //World Parameters
 
 //Physical constants
@@ -10,21 +20,36 @@ let R_coils = 0.07; // coil radius, m
 let n_coils = 320; // number of turns in coil
 let d_plates = 0.05; // separation of screen plates, metres
 let l_screen = 0.10; //length of screen, metres
+let aspect_ratio = l_screen/d_plates;
 let a_screen = 0; //angle of rotation of screen about vertical axis through centres of plates, degrees
+let pos_injection = {x:0, y: 0, z:0}; //position in plate region where particles enter
+let az_injection = 0; //azimuth angle for injection (degrees clockwise about y-axis)
+let alt_injection = 0; //altitude angle for injection (degrees ccw about z-axis)
+let dir_injection = {x: Math.cos(toRadians(alt_injection))*Math.cos(toRadians(az_injection)), y: Math.sin(toRadians(alt_injection)), z: Math.cos(toRadians(alt_injection))*Math.sin(toRadians(az_injection))};
+
+//coordinate setup for 'experiment region'. Its z-extent is the same as the distance between the plates.
+let exp_region = {
+    x: {min: 0, max: l_screen},
+    y: {min: -0.5*d_plates, max: 0.5*d_plates},
+    z: {min: -0.5*d_plates, max: 0.5*d_plates}
+}
+
 
 //Initial voltages. These are user adjustable.
 let V_accelerator = 1000; //where outlet is positive
-let V_plates = 1000; //where top is most positive
+let V_plates = 2000; //where top is most positive
 
 //Initial coil current. User adjustable.
-let I_coils = 2; 
+let I_coils = 0.53; 
 
 //time slowdown factor
-let time_slowdown = 1e-6;
+let time_slowdown = 1e-9;
 
 
 //number of calculation cycles per frame
 let physics_steps = 10;
+
+
 
 //field calculations
 //this could be written as vector equations or simply assume this is the field in the 'direction of uniformity'.
@@ -53,6 +78,21 @@ function calculate_charge_velocity (V, q, m) {
     let velocity = Math.sqrt(En_K*2/m);
     return velocity; 
 }
+
+// Generally helpful functions
+
+function toRadians (a) {
+    return (2*Math.PI*a/360);
+}
+
+let map_p5 = function (value, oldMin, oldMax, newMin, newMax) {
+  let prop = (value - oldMin)/(oldMax - oldMin);
+  let newVal = prop*(newMax - newMin) + newMin;
+  return newVal;
+}
+
+
+// function createParticles
 
 
 function Particle (options) {
@@ -105,18 +145,46 @@ Particle.prototype.update = function (fields = {E: {x:0,y:0,z:0}}, time_step = 1
     }
 }
 
+Particle.prototype.unalive = function () {
+    this.alive = false;
+}
+
+Particle.prototype.render = function (target_width = l_screen, target_height = d_plates) {
+    //convert natural position to pixel position.
+    ///first, get the natural position as a percentage of screen position.
+    let pos_rel = {x:0, y:0, z:0};
+    for (let dir in pos_rel) {
+        pos_rel[dir] = map_p5(this.pos[dir], exp_region[dir].min, exp_region[dir].max, 0, 1);
+    }
+    ctx_exp.beginPath();
+    ctx_exp.fillStyle = 'rgb(255,255,0)';
+    ctx_exp.ellipse(pos_rel.x*canvas_exp.width, pos_rel.y*canvas_exp.height, 5, 5, 0, 0, 2*Math.PI);
+    ctx_exp.stroke();
+}
+
+
+//Initialise simulation
+
 let fieldies = calculate_fields();
 // let electron_speed = calculate_charge_velocity(V_accelerator, q_e, m_e);
 let party = new Particle();
-party.vel.x = calculate_charge_velocity(V_accelerator, party.q, party.mass);
+let party_speed = calculate_charge_velocity(V_accelerator, party.q, party.mass);
+for (let dir in dir_injection) {
+    party.vel[dir] = party_speed*dir_injection[dir];
+}
 
+console.log(party.vel);
 
 function animate () {
+
+    // ctx_exp.clearRect(0,0, canvas_exp.width, canvas_exp.height);
+
     for (let i  = 0; i < physics_steps; i++) {
         party.update(fieldies, (1/60)*time_slowdown/physics_steps);
     }
 
-    console.log(party.vel);
+    party.render();
+
     requestAnimationFrame(animate);
 }
 
