4dddcced22ed468a1526b149ffbe19867e2a2b29
Tristan Curry <7457323+tristancurry@users.noreply.github.com>
2023-08-20 11:23:11 +0930

    messing with parameters
    auto-calculate current required for force equalisation

--- scripts/main.js
+++ scripts/main.js
@@ -22,10 +22,10 @@ let d_plates = 0.05; // separation of screen plates, metres
 let l_screen = 0.10; //length of screen, metres
 let aspect_ratio = l_screen/d_plates;
 let a_screen = 0; //angle of rotation of screen about vertical axis through centres of plates, degrees
-let pos_injection = {x:0, y: 0, z:0}; //position in plate region where particles enter
-let az_injection = 0; //azimuth angle for injection (degrees clockwise about y-axis)
+let pos_injection = {x:0, y: 0.0, z:0}; //position in plate region where particles enter
+let az_injection = 35; //azimuth angle for injection (degrees clockwise about y-axis)
 let alt_injection = 0; //altitude angle for injection (degrees ccw about z-axis)
-let dir_injection = {x: Math.cos(toRadians(alt_injection))*Math.cos(toRadians(az_injection)), y: Math.sin(toRadians(alt_injection)), z: Math.cos(toRadians(alt_injection))*Math.sin(toRadians(az_injection))};
+let dir_injection = {x: Math.cos(toRadians(alt_injection))*Math.cos(toRadians(az_injection)), y: -1*Math.sin(toRadians(alt_injection)), z: Math.cos(toRadians(alt_injection))*Math.sin(toRadians(az_injection))};
 
 //coordinate setup for 'experiment region'. Its z-extent is the same as the distance between the plates.
 let exp_region = {
@@ -40,14 +40,17 @@ let V_accelerator = 1000; //where outlet is positive
 let V_plates = 2000; //where top is most positive
 
 //Initial coil current. User adjustable.
-let I_coils = 0.53; 
+let I_coils = 0.519; 
+
+let I_req = Math.sqrt(m_e/(2*V_accelerator*q_e))*V_plates*R_coils/((0.8**1.5)*mu_0*n_coils*d_plates);
+console.log(I_req);
 
 //time slowdown factor
 let time_slowdown = 1e-9;
 
 
 //number of calculation cycles per frame
-let physics_steps = 10;
+let physics_steps = 100;
 
 
 
@@ -157,7 +160,7 @@ Particle.prototype.render = function (target_width = l_screen, target_height = d
         pos_rel[dir] = map_p5(this.pos[dir], exp_region[dir].min, exp_region[dir].max, 0, 1);
     }
     ctx_exp.beginPath();
-    ctx_exp.fillStyle = 'rgb(255,255,0)';
+    ctx_exp.strokeStyle = 'rgb(255,255,255)';
     ctx_exp.ellipse(pos_rel.x*canvas_exp.width, pos_rel.y*canvas_exp.height, 5, 5, 0, 0, 2*Math.PI);
     ctx_exp.stroke();
 }
@@ -167,7 +170,7 @@ Particle.prototype.render = function (target_width = l_screen, target_height = d
 
 let fieldies = calculate_fields();
 // let electron_speed = calculate_charge_velocity(V_accelerator, q_e, m_e);
-let party = new Particle();
+let party = new Particle({pos: pos_injection});
 let party_speed = calculate_charge_velocity(V_accelerator, party.q, party.mass);
 for (let dir in dir_injection) {
     party.vel[dir] = party_speed*dir_injection[dir];
@@ -177,7 +180,7 @@ console.log(party.vel);
 
 function animate () {
 
-    // ctx_exp.clearRect(0,0, canvas_exp.width, canvas_exp.height);
+    ctx_exp.clearRect(0,0, canvas_exp.width, canvas_exp.height);
 
     for (let i  = 0; i < physics_steps; i++) {
         party.update(fieldies, (1/60)*time_slowdown/physics_steps);
