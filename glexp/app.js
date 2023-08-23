1c19453254a584f1d19a4dcf29cfc8acd99ae1b5
Tristan Curry <7457323+tristancurry@users.noreply.github.com>
2023-08-23 14:39:13 +0930

    experimenting with three.js

--- glexp/app.js
+++ glexp/app.js
@@ -1,110 +1,40 @@
-var canvas = document.getElementById("canvas");
-  
-// Create a WebGL rendering context  
-var gl = canvas.getContext("webgl2");
-  
-// Tell user if their browser does not support WebGL
-if (!gl) {
-    alert("Your browser does not support WebGL");
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+//Scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#00b140');
+
+//Camera
+const fov = 45 //Field of View
+const aspect = window.innerWidth/window.innerHeight;
+const near = 0.1;
+const far = 100;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.set(0, 0, 10);
+
+//Renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+//Cube makin
+const geometry = new THREE.BoxGeometry(2, 2, 2);
+const material = new THREE.MeshBasicMaterial({ wireframe: true});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+//Rendering
+const container = document.querySelector('#threejs-container');
+container.append(renderer.domElement);
+renderer.render(scene, camera);
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    renderer.render(scene, camera);
 }
-  
-// Set the color of the canvas.
-// Parameters are RGB colors (red, green, blue, alpha)
-gl.clearColor(0, 0.6, 0.0, 1.0);
-// Clear the color buffer with specified color
-gl.clear(gl.COLOR_BUFFER_BIT);
 
-//Define shaders
-const shaders = {
-    vs: `#version 300 es
-        in vec2 vertPosition;
-        in vec3 vertColor;
-        out vec3 fragColor;
-      
-        void main() {
-            fragColor = vertColor;
-            gl_Position = vec4(vertPosition, 0, 1);
-        }`,
-  
-    fs: `#version 300 es
-        precision mediump float;
-        in vec3 fragColor;
-        out vec4 outColor;
-      
-        void main() {
-            outColor = vec4(fragColor, 1);
-        }`
-};
+animate();
 
-//create WebGL shader objects
-var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-
-// sets the source code of the WebGL shader
-gl.shaderSource(vertexShader, shaders.vs);
-gl.shaderSource(fragmentShader, shaders.fs);
-  
-// Compile GLSL Shaders to a binary data 
-// so WebGLProgram can use them
-gl.compileShader(vertexShader);
-gl.compileShader(fragmentShader);
-
-
-var program = gl.createProgram();
-
-gl.attachShader(program, vertexShader);
-gl.attachShader(program, fragmentShader);
-
-gl.linkProgram(program);
-
-
-//making the shape.
-
-const vertexAttributes = {
-    position: {
-        numberOfComponents: 2, //X and Y paired coords
-        data: new Float32Array([0.0, 0.5, -0.5, -0.5, 0.5, -0.5])
-    },
-
-    color: {
-        numberOfComponents: 3, //RGB triple
-        data: new Float32Array([1, 0, 0, 0, 1, 0, 0, 0, 1])
-    }
-};
-
-
-
-// Create an initialize vertex buffers
-var vertexBufferObjectPosition = gl.createBuffer();
-var vertexBufferObjectColor = gl.createBuffer();
-  
-// Bind existing attribute data
-gl.bindBuffer(gl.ARRAY_BUFFER, vertexBufferObjectPosition);
-gl.bufferData(gl.ARRAY_BUFFER, vertexAttributes.position.data, 
-        gl.STATIC_DRAW);
-  
-var positionAttribLocation = gl.getAttribLocation(program, 
-        'vertPosition');
-  
-gl.vertexAttribPointer(positionAttribLocation,
-    vertexAttributes.position.numberOfComponents, 
-        gl.FLOAT, gl.FALSE, 0, 0);
-gl.enableVertexAttribArray(positionAttribLocation);
-  
-// Bind existing attribute data
-gl.bindBuffer(gl.ARRAY_BUFFER, vertexBufferObjectColor);
-gl.bufferData(gl.ARRAY_BUFFER, vertexAttributes.color.data, 
-        gl.STATIC_DRAW);
-  
-var colorAttribLocation = gl.getAttribLocation(program, 
-        'vertColor');
-  
-gl.vertexAttribPointer(colorAttribLocation,
-    vertexAttributes.color.numberOfComponents, gl.FLOAT, 
-            gl.FALSE, 0, 0);
-gl.enableVertexAttribArray(colorAttribLocation);
-  
-// Set program as part of the current rendering state
-gl.useProgram(program);
-// Draw the triangle
-gl.drawArrays(gl.TRIANGLES, 0, 3);
+const controls = new OrbitControls(camera, renderer.domElement)
